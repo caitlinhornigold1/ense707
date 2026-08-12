65f6dd1d550c86a3dd381d4d5ddbadb3d3ddab88
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2026-08-12 13:05:48 +1200

    Starter Impl
    This push adds the detection class, it contains a single task ScanAsync which takes a string input. the purpose of this is to return the number of image elements are missing alt tags or have empty ones.

---
+++ AccessibilityAnalyser.altdetect/Detection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AccessibilityAnalyser.Core;
+
+namespace AccessibilityAnalyser
+{
+    public class Detection
+    {
+        public int MissingAlts { get; private set; }
+
+        public async Task<int> ScanAsync(string inputURI)
+        {
+            // Get page
+            var fetcher = new SourceFetcher();
+            var html = await fetcher.GetHtmlAsync(inputURI);
+
+            // Get all <img>
+            var imgMatches = Regex.Matches(html, @"<img\b[^>]*>", RegexOptions.IgnoreCase);
+            int count = 0;
+
+            foreach (Match img in imgMatches)
+            {
+                var imgTag = img.Value;
+
+                // Match alt="value" or alt='value'
+                var altMatch = Regex.Match(imgTag, @"alt\s*=\s*([""'])(.*?)\1", RegexOptions.IgnoreCase);
+
+                if (!altMatch.Success)
+                {
+                    // No alt attribute present at all
+                    count++;
+                }
+                else if (string.IsNullOrWhiteSpace(altMatch.Groups[2].Value))
+                {
+                    // Alt attribute exists but is empty
+                    count++;
+                }
+            }
+
+            MissingAlts = count;
+            return count;
+        }
+    }
+}
