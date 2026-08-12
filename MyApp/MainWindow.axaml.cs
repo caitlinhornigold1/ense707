479bcf605aa6a8563526a8d6f0068c20efaf8808
caitlinhornigold1 <caitlin.hornigold@gmail.com>
2026-08-12 13:12:19 +1200

    window gui can accept url

--- MyApp/MainWindow.axaml.cs
+++ MyApp/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using AccessibilityAnalyser.Core;
 
 namespace MyApp;
 
@@ -7,5 +10,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void AnalyseWebsite_Click(
+        object? sender,
+        RoutedEventArgs e)
+    {
+        string url = WebsiteUrlTextBox.Text ?? "";
+
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            return;
+        }
+
+        var fetcher = new SourceFetcher();
+
+        string html = await fetcher.GetHtmlAsync(url);
+
+        var parser = new HtmlParser();
+
+        var document = await parser.ParseAsync(html);
+
+        // Website is now parsed.
+        Console.WriteLine("HTML successfully parsed!");
     }
 }
