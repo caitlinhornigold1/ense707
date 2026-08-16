777a750825a9fc115bb8b09b97a2663aaa2b02e5
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2026-08-16 18:55:05 +1200

    Start main test script for report generation

---
+++ AccessibilityAnalyser.Core/Report.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AccessabilityAnalyser;
+
+public class Report
+{
+	double finalScore;
+	string uri;
+	public Report(string uri, string html)
+	{
+		uri = uri;
+		finalScore = 0.0;
+		var html = html;
+	}
+
+	public Report GenerateReport(string uri, ...)
+	{
+        var fetcher = new SourceFetcher();
+        var html = await fetcher.GetHtmlAsync(uri);
+
+		Report currentReport = new Report(uri, html);
+
+		int AltsResult = AccessabilityAnalyser.altdetect ScanAsync(html);
+		switch(AltsResult)
+			case: 0
+				currentReport.finalScore += 15.0;
+				break;		
+			case: 1
+				currentReport.finalScore += 10.0
+				break;
+			case: >1
+				currentReport.finalScore -= 5.0
+				break;
+			
+
+
+    }
+}
