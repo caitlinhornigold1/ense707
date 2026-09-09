cec09cd2e1ba6ee4e54b1554fd7660b2b2730430
WEEZOOKA <76672483+WEEZOOKARP@users.noreply.github.com>
2026-09-09 12:48:05 +1200

    Test script, unable to properly test
    All compilations suddenly fail with this error
    1>------ Build started: Project: AccessibilityAnalyser.Core, Configuration: Debug ------
    2>------ Build started: Project: AccessibilityAnalyser.altdetect, Configuration: Debug ------
    2>  AccessibilityAnalyser.altdetect -> C:\Users\weezo\Desktop\QA\ense707\AccessibilityAnalyser.altdetect\bin\Debug\net10.0\AccessibilityAnalyser.altdetect.dll
    1>  AccessibilityAnalyser.Core -> C:\Users\weezo\Desktop\QA\ense707\AccessibilityAnalyser.Core\bin\Debug\net10.0\AccessibilityAnalyser.Core.dll
    3>------ Build started: Project: AccessibilityAnalyser.Tests, Configuration: Debug ------
    3>C:\Program Files\dotnet\sdk\10.0.302\Sdks\Microsoft.NET.Sdk\targets\Microsoft.PackageDependencyResolution.targets(266,5): error NETSDK1004: Assets file 'C:\Users\weezo\Desktop\QA\ense707\AccessibilityAnalyser.Tests\obj\project.assets.json' not found. Run a NuGet package restore to generate this file.
    ========== Build: 2 succeeded, 1 failed, 0 up-to-date, 0 skipped ==========
    Pushing code so that someone else may be able to test it while I resolve whatever demons have infested my visual studio

---
+++ AccessibilityAnalyser.Tests/encapsulationtests.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AccessibilityAnalyser.Core;
+using Xunit;
+
+namespace AccessibilityAnalyser.Tests;
+
+public class EncapsulationTests
+{
+    [Fact]
+    public async Task UnclosedElement_ReturnsUnclosedDivTag()
+    {
+        var html = @"
+            <html><body>
+                <div id='header'>
+                <div id='content'></div>
+                <div id='header'></div>
+                <span id='content'></span>
+            </body></html>";
+
+        var analyzer = new malformedHtml();
+        var expected = new List<string> { "div" };
+
+        IReadOnlyList<string> result = await analyzer.ScanAsync(html);
+
+        Assert.Equal(expected, result);
+    }
+}
